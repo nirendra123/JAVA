b49fd315e03ba4e656f2e89a21074ea0aa9fd492
nirendra29 <nnirendrabajracharya29@gmail.com>
2023-04-13 14:12:35 +0545

    insertion,update,delete in database

--- Database_Connetction/src/Main.java
+++ Database_Connetction/src/Main.java
@@ -3,27 +3,55 @@ import java.sql.*;
 import static java.lang.Class.forName;
 
 public class Main {
-    public static void main(String[] args)  {
-        try {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//        try {
+//
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/database", "root", "root");
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("select * from table1");
+//
+//            while (resultSet.next()) {
+//                int id = resultSet.getInt("id");
+//                String name = resultSet.getString("name");
+//                String address = resultSet.getString("address");
+//                System.out.println(id + " " + name + " " + address);
+//            }
+//            connection.close();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//
+//
+//helps to use jar file
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost/database";
+        Connection connection= DriverManager.getConnection(url, "root", "root");
+        System.out.println("connection succesful");
 
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/database", "root", "root");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from table1");
 
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String address = resultSet.getString("address");
-                System.out.println(id + " " + name + " " + address);
-            }
-            connection.close();
-        }catch (Exception e){
-            e.printStackTrace();
+
+        //create the query
+        Statement statement=connection.createStatement();
+        ResultSet resultSet=statement.executeQuery("select * from table1");
+        //System.out.println(resultSet.next());
+        while(resultSet.next()){
+          String name=  resultSet.getString("name");
+            System.out.println(name);
         }
-    }
 
 
 
     }
+}
+
+
+
+
+
+
+
 
--- Games/TicTacToe/src/TicTacToe.java
+++ Games/TicTacToe/src/TicTacToe.java
@@ -6,19 +6,20 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Random;
 
-public class TicTacToe implements ActionListener , MouseListener {
+public class TicTacToe implements ActionListener {
 
-    Random random=new Random();
-    JFrame jFrame=new JFrame("TicTacToe");
-    JButton jButton=new JButton();
-    JPanel title_Panel=new JPanel();
-    JPanel button_pannel=new JPanel();
-    JLabel jLabel=new JLabel();
-    JButton[] buttons=new JButton[9];
+    Random random = new Random();
+    JFrame jFrame = new JFrame("TicTacToe");
+    JButton jButton = new JButton();
+    JPanel title_Panel = new JPanel();
+    JPanel button_pannel = new JPanel();
+    JLabel jLabel = new JLabel();
+    JButton[] buttons = new JButton[9];
     boolean player1_turn;
-    TicTacToe(){
+
+    TicTacToe() {
         jFrame.setLayout(new BorderLayout());
-        jFrame.setSize(800,800);
+        jFrame.setSize(800, 800);
         jFrame.getContentPane().setBackground(Color.BLACK);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setResizable(false);
@@ -26,13 +27,13 @@ public class TicTacToe implements ActionListener , MouseListener {
 
         jLabel.setBackground(Color.WHITE);
         jLabel.setForeground(Color.GREEN);
-        jLabel.setFont(new Font("Ink Free",Font.BOLD,75));
+        jLabel.setFont(new Font("Ink Free", Font.BOLD, 75));
         jLabel.setHorizontalAlignment(JLabel.CENTER);
         jLabel.setText("Tic-Tac-Toe");
-       jLabel.setOpaque(true);
+        jLabel.setOpaque(true);
 
         title_Panel.setLayout(new BorderLayout());
-        title_Panel.setBounds(0,0,800,100);
+        title_Panel.setBounds(0, 0, 800, 100);
 
         title_Panel.add(jLabel);
         jFrame.add(title_Panel);
@@ -40,56 +41,34 @@ public class TicTacToe implements ActionListener , MouseListener {
 
     }
 
-    public void firstTurn(){
-
-
-
-    }
-    public void check(){
+    public void firstTurn() {
 
 
     }
 
-    public void xWins(int a ,int b,int c){
+    public void check() {
 
-    }
-    public void yWins(){
 
     }
 
-   public void move(int i){
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void yWins() {
+
+    }
+
+    public void move(int i) {
 
 
-   }
+    }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
 }
+
+
