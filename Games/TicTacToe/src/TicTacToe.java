dfa70ca7ab3efe762d6d23f94c80b6787b0507b6
nirendra29 <nnirendrabajracharya29@gmail.com>
2023-04-14 22:29:02 +0545

    added gui

--- Games/TicTacToe/src/TicTacToe.java
+++ Games/TicTacToe/src/TicTacToe.java
@@ -9,39 +9,71 @@ import java.util.Random;
 public class TicTacToe implements ActionListener {
 
     Random random = new Random();
-    JFrame jFrame = new JFrame("TicTacToe");
+    JFrame jFrame = new JFrame();
     JButton jButton = new JButton();
     JPanel title_Panel = new JPanel();
-    JPanel button_pannel = new JPanel();
+    JPanel button_panel = new JPanel();
     JLabel jLabel = new JLabel();
     JButton[] buttons = new JButton[9];
     boolean player1_turn;
 
     TicTacToe() {
-        jFrame.setLayout(new BorderLayout());
-        jFrame.setSize(800, 800);
-        jFrame.getContentPane().setBackground(Color.BLACK);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setResizable(false);
+        jFrame.setSize(500, 500);
+        jFrame.setLayout(new BorderLayout());
+        jFrame.getContentPane().setBackground(new Color(50,50,50));
         jFrame.setVisible(true);
 
-        jLabel.setBackground(Color.WHITE);
-        jLabel.setForeground(Color.GREEN);
-        jLabel.setFont(new Font("Ink Free", Font.BOLD, 75));
-        jLabel.setHorizontalAlignment(JLabel.CENTER);
-        jLabel.setText("Tic-Tac-Toe");
-        jLabel.setOpaque(true);
 
-        title_Panel.setLayout(new BorderLayout());
-        title_Panel.setBounds(0, 0, 800, 100);
+
+         jLabel.setBackground(new Color(25,25,25));
+         jLabel.setForeground(new Color(25,255,0));
+         jLabel.setFont(new Font("Ink Free", Font.BOLD, 75));
+         jLabel.setHorizontalAlignment(JLabel.CENTER);
+         jLabel.setText("Tic-Tac-Toe");
+         jLabel.setOpaque(true);
+
+         title_Panel.setLayout(new BorderLayout());
+         title_Panel.setBounds(0, 0, 500, 100);
+
+         button_panel.setLayout(new GridLayout(3,3));
+         button_panel.setBackground(new Color(150,150,150));
+
+         for(int i=0;i<9;i++){
+             buttons[i]=new JButton();
+             button_panel.add(buttons[i]);
+             buttons[i].setFont(new Font("My Boli",Font.BOLD,120));
+             buttons[i].setFocusable(false);
+             buttons[i].addActionListener(this);
+             firstTurn();
+
+         }
+
+
 
         title_Panel.add(jLabel);
-        jFrame.add(title_Panel);
+        jFrame.add(title_Panel,BorderLayout.NORTH);
+        jFrame.add(button_panel);
+
 
 
     }
 
     public void firstTurn() {
+//        try {
+//            Thread.sleep(200);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        if(random.nextInt(2)==0){
+            player1_turn=true;
+            jLabel.setText("X turn");
+        }else {
+            player1_turn=false;
+            jLabel.setText("O turn");
+
+        }
+
 
 
     }
@@ -55,7 +87,7 @@ public class TicTacToe implements ActionListener {
 
     }
 
-    public void yWins() {
+    public void oWins(int a,int b,int c) {
 
     }
 
