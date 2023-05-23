bb73df6110282ace04ba6a93430c73c7b5beed05
nirendra123 <nnirendrabajracharya13@gmail.com>
2023-05-23 22:18:05 +0545

    multiple Inheritance

---
+++ Inheritance/tryMultiple Inheritance/src/Main.java
@@ -0,0 +1,34 @@
+//class A{
+//
+//}
+//class B{
+//
+//}
+//class C extends A,B{
+//    multiple inheritance doesnot work in java
+//but why/
+//
+
+//}
+//but there is a indirect way ie with the help of interfaces
+
+
+
+
+
+
+
+
+
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
