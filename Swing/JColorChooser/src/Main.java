cfb5ceea76e0a255a209ea74e5e82f8780eb05d4
nirendra123 <nnirendrabajracharya13@gmail.com>
2023-05-24 00:11:14 +0545

    JColorChooser

---
+++ Swing/JColorChooser/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       //JcolorChooser= A GUI mechanism that let's  a user to choose a color
+
+        new MyFrame();
+    }
+}
---
+++ Swing/JColorChooser/src/MyFrame.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+   JButton button;
+   JLabel label;
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button=new JButton("Pick a color");
+        button.addActionListener(this);
+
+        label=new JLabel();
+        label.setBackground(Color.WHITE);
+        label.setText("This is a text");
+        label.setFont(new Font("MV Boli",Font.PLAIN,100));
+        label.setOpaque(true);
+
+
+        this.add(button);
+        this.add(label);
+        this.pack();
+        this.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button){
+           JColorChooser colorChooser=new JColorChooser();
+           Color color=JColorChooser.showDialog(null,"pick a color i guss",Color.black);
+           //label.setForeground(color);
+            label.setBackground(color);
+        }
+
+    }
+}
